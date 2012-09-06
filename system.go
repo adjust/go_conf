634b8f7e852d5a8aa17496a05d1e417f80bd8f9e
Paul H. Müller <phm@rapidrabbit.de>
2012-09-06 18:01:14 +0200

    removed condition

--- system.go
+++ system.go
@@ -22,16 +22,14 @@ func signalCatcher() {
 func DropPrivileges(user_name string) {
 	//drop privileges only if we are on the servers.
 	if os.Getenv("GO_ENV") == "production" {
-		if syscall.Getuid() == 0 {
-			usr, err := user.Lookup(user_name)
-			if err != nil {
-				panic(err)
-			}
-			id, _ := strconv.Atoi(usr.Uid)
-			err = syscall.Setuid(id)
-			if err != nil {
-				panic(err)
-			}
+		usr, err := user.Lookup(user_name)
+		if err != nil {
+			panic(err)
+		}
+		id, _ := strconv.Atoi(usr.Uid)
+		err = syscall.Setuid(id)
+		if err != nil {
+			panic(err)
 		}
 	}
 }
