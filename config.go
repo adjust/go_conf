629134935c228007de197662784bc4831d9ad638
Paul H. Müller <phm@rapidrabbit.de>
2012-09-27 17:14:02 +0200

    new conf parameter for special case with 2 redis confs

--- config.go
+++ config.go
@@ -71,6 +71,16 @@ func GetRedisConf() (redis_host string, redis_db int) {
 	return
 }
 
+func GetMasterRedisConf() (redis_host string, redis_db int) {
+	redis_host = getConfigParameter("master_redis", "host")
+	db_str := getConfigParameter("master_redis", "db")
+	redis_db, err := strconv.Atoi(db_str)
+	if err != nil {
+		log.Panic("master redis db not an integer!")
+	}
+	return
+}
+
 func GetPgConf() string {
 	pg_user := getConfigParameter("postgres", "user")
 	pg_db := getConfigParameter("postgres", "db")
